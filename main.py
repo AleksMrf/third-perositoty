8ac46bc1bc70501959951f3e02bc4618b770ed3a
AleksMrf <a.n.marufenko@gmail.com>
2024-10-12 08:00:03 +0300

    new code

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 def add(x, y):
-    """Функция для сложения двух чисел."""
     return x + y
 
+def subtract(x, y):
+    return x - y
 
 def calculator():
-    print("Простой калькулятор")
-    print("Доступная операция: Сложение")
+    print("Выберите операцию:")
+    print("1. Сложение")
+    print("2. Вычитание")
 
-    num1 = float(input("Введите первое число: "))
-    num2 = float(input("Введите второе число: "))
+    choice = input("Введите номер операции (1/2): ")
 
-    result = add(num1, num2)
-    print(f"{num1} + {num2} = {result}")
+    if choice in ['1', '2']:
+        num1 = float(input("Введите первое число: "))
+        num2 = float(input("Введите второе число: "))
 
+        if choice == '1':
+            print(f"{num1} + {num2} = {add(num1, num2)}")
+        elif choice == '2':
+            print(f"{num1} - {num2} = {subtract(num1, num2)}")
+    else:
+        print("Неверный ввод. Пожалуйста, выберите 1 или 2.")
 
 if __name__ == "__main__":
     calculator()
